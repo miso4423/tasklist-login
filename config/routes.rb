a793ffc5a03d18f71b1a31d05632ba4a7276beca
miso <kfaqn77723@yahoo.co.jp>
2020-11-10 11:11:04 +0000

    WIP in Lesson15

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
  
-  get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+ 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: "tasks#index"
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "login", to: "sessions#destroy"
+  delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks, only: [:index, :show, :new, :create]
 end
