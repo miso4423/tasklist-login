3c0ad3a412c30073ac3b90de949067fbb97ff747
miso <kfaqn77723@yahoo.co.jp>
2020-11-10 11:11:49 +0000

    Revert "WIP in Lesson15"
    This reverts commit a793ffc5a03d18f71b1a31d05632ba4a7276beca.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
   def index
-    @user = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
  
- 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: "tasks#index"
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  delete "login", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
 end
