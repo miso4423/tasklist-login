4be9a0461b7b774a5425c3335c3f7062590cef37
miso <kfaqn77723@yahoo.co.jp>
2020-11-10 11:12:47 +0000

    Revert "signup"
    This reverts commit 2a5a76a5d3cf61fd2c8fc2c0da44cb24a2ee8957.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
- 
-  get 'toppages/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   root to: "tasks#index"
-  
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  delete "login", to: "sessions#destroy"
-  
-  get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
+  resources :tasks
 end
