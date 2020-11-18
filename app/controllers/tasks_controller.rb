8da9f292d77fac52bb159f3eb846aa2e638d5139
miso <kfaqn77723@yahoo.co.jp>
2020-11-18 10:40:36 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user ,only:[:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -11,11 +11,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクが正常に記録されました"
       redirect_to @task
@@ -50,12 +50,17 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
